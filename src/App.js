32f11904bed16a858ac3dc29155f9fca140e0677
surajkunwarkcgi <kunwarsuraj2019@gmail.com>
2023-05-07 16:07:24 +0900

    Chat functionality is added.Project complete

--- src/App.js
+++ src/App.js
@@ -1,30 +1,97 @@
-import { useEffect, useState } from 'react'
-import { ethers } from 'ethers'
-import { io } from "socket.io-client"
+import { useEffect, useState } from "react";
+import { ethers } from "ethers";
+import { io } from "socket.io-client";
 
 // Components
-import Navigation from './components/Navigation'
-import Servers from './components/Servers'
-import Channels from './components/Channels'
-import Messages from './components/Messages'
+import Navigation from "./components/Navigation";
+import Servers from "./components/Servers";
+import Channels from "./components/Channels";
+import Messages from "./components/Messages";
 
 // ABIs
-import Dappcord from './abis/Dappcord.json'
+import Dappcord from "./abis/Dappcord.json";
 
 // Config
-import config from './config.json';
+import config from "./config.json";
 
 // Socket
-const socket = io('ws://localhost:3030');
+const socket = io("ws://localhost:3030");
 
 function App() {
+  const [account, setAccount] = useState(null);
+  const [provider, setProvider] = useState(null);
+  const [dappcord, setDappcord] = useState(null);
+  const [channels, setchannels] = useState([]);
+  const [messages, setMessages] = useState([]);
+  const [currentChannel, setCurrentChannel] = useState(null);
+
+  const loadBlockchainData = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    setProvider(provider);
+
+    const network = await provider.getNetwork();
+    const dappcord = new ethers.Contract(
+      config[network.chainId].Dappcord.address,
+      Dappcord,
+      provider
+    );
+    setDappcord(dappcord);
+
+    const totalChannels = await dappcord.totalChannels();
+    const channels = [];
+
+    for (var i = 1; i <= totalChannels; i++) {
+      const channel = await dappcord.getChannel(i);
+      channels.push(channel);
+    }
+    setchannels(channels);
+
+    window.ethereum.on("accountsChanged", async () => {
+      window.location.reload();
+    });
+  };
+
+  useEffect(() => {
+    loadBlockchainData();
+
+    socket.on("connect", () => {
+      socket.emit("get messages");
+    });
+
+    socket.on("new message", (messages) => {
+      setMessages(messages);
+    });
+
+    socket.on("get messages", (messages) => {
+      setMessages(messages);
+    });
+
+    return () => {
+      socket.off("connect");
+      socket.off("new message");
+      socket.off("get messages");
+    };
+  }, []);
 
   return (
     <div>
-      <h1 style={{ textAlign: "center", padding: "15px" }}>Welcome to Dappcord</h1>
+      <Navigation account={account} setAccount={setAccount} />
 
       <main>
-
+        <Servers />
+        <Channels
+          provider={provider}
+          account={account}
+          dappcord={dappcord}
+          channels={channels}
+          currentChannel={currentChannel}
+          setCurrentChannel={setCurrentChannel}
+        />
+        <Messages
+          account={account}
+          messages={messages}
+          currentChannel={currentChannel}
+        />
       </main>
     </div>
   );
